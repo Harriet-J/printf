afd812738c860620d04e94bc30de2dab0f903c67
Meryem <ml.meryem.16@gmail.com>
2024-07-30 11:57:43 -0700

    add header file

---
+++ main.h
@@ -0,0 +1,12 @@
+// main.h
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+// Function prototype for the custom printf function
+int _printf(const char *format, ...);
+
+#endif // main_h
+
