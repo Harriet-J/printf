12c3ff218f0c9160eb9ab825c1812cd775cbe7a2
Harriet-J <seremharriet@gmail.com>
2024-07-30 12:12:48 -0700

    Implement printf function

--- _printf.c
+++ _printf.c
@@ -1,54 +1,51 @@
-#include <stdio.h>   // Include standard I/O library for putchar
-#include <stdarg.h>  // Include standard argument library for va_list, va_start, va_arg, and va_end
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
 
-// Function prototype for _printf
-int _printf(const char *format, ...) {
-    va_list args;              // Declare a variable to hold the variable arguments
-    int printed_chars = 0;     // Variable to count the number of characters printed
-    char *str;                 // Pointer to a string for the %s specifier
-    char ch;                   // Variable to hold a character for the %c specifier
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int i = 0, count = 0;
+    char c;
+    char *s;
 
-    // Initialize the va_list to retrieve additional arguments after 'format'
     va_start(args, format);
 
-    // Loop through each character in the format string
-    for (const char *ptr = format; *ptr != '\0'; ptr++) {
-        if (*ptr == '%') {     // Check if the current character is a '%'
-            ptr++;             // Move to the next character to check the specifier
-            switch (*ptr) {    // Switch based on the specifier character
-                case 'c':      // Handle %c specifier
-                    ch = (char) va_arg(args, int); // Retrieve the character argument
-                    putchar(ch);                   // Print the character
-                    printed_chars++;               // Increment the character count
+    while (format && format[i])
+    {
+        if (format[i] == '%' && format[i + 1])
+        {
+            i++;
+            switch (format[i])
+            {
+                case 'c':
+                    c = va_arg(args, int);
+                    count += write(1, &c, 1);
                     break;
-                case 's':      // Handle %s specifier
-                    str = va_arg(args, char *);    // Retrieve the string argument
-                    for (; *str != '\0'; str++) {  // Loop through each character in the string
-                        putchar(*str);             // Print the character
-                        printed_chars++;           // Increment the character count
+                case 's':
+                    s = va_arg(args, char *);
+                    while (*s)
+                    {
+                        count += write(1, s, 1);
+                        s++;
                     }
                     break;
-                case '%':      // Handle %% specifier
-                    putchar('%');                  // Print a literal '%'
-                    printed_chars++;               // Increment the character count
+                case '%':
+                    count += write(1, &format[i], 1);
                     break;
-                default:       // Handle unexpected format specifiers
-                    putchar('%');                  // Print the '%' character
-                    putchar(*ptr);                 // Print the unknown specifier character
-                    printed_chars += 2;            // Increment the count for two characters
-                    break;
+                default:
+                    count += write(1, &format[i - 1], 1);
+                    count += write(1, &format[i], 1);
             }
-        } else {
-            // If the character is not '%', print it as-is
-            putchar(*ptr);
-            printed_chars++;
         }
+        else
+        {
+            count += write(1, &format[i], 1);
+        }
+        i++;
     }
 
-    // Clean up the va_list when done
     va_end(args);
-
-    // Return the total number of characters printed
-    return printed_chars;
+    return count;
 }
 
--- main.h
+++ main.h
@@ -1,12 +1,7 @@
-// main.h
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdarg.h>
-
-// Function prototype for the custom printf function
 int _printf(const char *format, ...);
 
-#endif // main_h
+#endif /* MAIN_H */
 
