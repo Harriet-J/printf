14bf652b9613052c14a6495e7c853310f2d43d0f
Meryem <ml.meryem.16@gmail.com>
2024-08-02 06:10:04 -0700

    handle Null in %s

--- _printf.c
+++ _printf.c
@@ -1,54 +1,97 @@
-#include <stdio.h>   // Include standard I/O library for putchar
-#include <stdarg.h>  // Include standard argument library for va_list, va_start, va_arg, and va_end
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
 
-// Function prototype for _printf
-int _printf(const char *format, ...) {
-    va_list args;              // Declare a variable to hold the variable arguments
-    int printed_chars = 0;     // Variable to count the number of characters printed
-    char *str;                 // Pointer to a string for the %s specifier
-    char ch;                   // Variable to hold a character for the %c specifier
+/**
+ * _printf - produces output according to a format
+ * @format: character string composed of zero or more directives
+ * Return: the number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0, count = 0;
+	char c;
+	char *s;
+	int d;
 
-    // Initialize the va_list to retrieve additional arguments after 'format'
-    va_start(args, format);
+	va_start(args, format);
 
-    // Loop through each character in the format string
-    for (const char *ptr = format; *ptr != '\0'; ptr++) {
-        if (*ptr == '%') {     // Check if the current character is a '%'
-            ptr++;             // Move to the next character to check the specifier
-            switch (*ptr) {    // Switch based on the specifier character
-                case 'c':      // Handle %c specifier
-                    ch = (char) va_arg(args, int); // Retrieve the character argument
-                    putchar(ch);                   // Print the character
-                    printed_chars++;               // Increment the character count
-                    break;
-                case 's':      // Handle %s specifier
-                    str = va_arg(args, char *);    // Retrieve the string argument
-                    for (; *str != '\0'; str++) {  // Loop through each character in the string
-                        putchar(*str);             // Print the character
-                        printed_chars++;           // Increment the character count
-                    }
-                    break;
-                case '%':      // Handle %% specifier
-                    putchar('%');                  // Print a literal '%'
-                    printed_chars++;               // Increment the character count
-                    break;
-                default:       // Handle unexpected format specifiers
-                    putchar('%');                  // Print the '%' character
-                    putchar(*ptr);                 // Print the unknown specifier character
-                    printed_chars += 2;            // Increment the count for two characters
-                    break;
-            }
-        } else {
-            // If the character is not '%', print it as-is
-            putchar(*ptr);
-            printed_chars++;
-        }
+	while (format && format[i])
+	{
+		if (format[i] == '%' && format[i + 1])
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					c = va_arg(args, int);
+					count += write(1, &c, 1);
+					break;
+				case 's':
+    s = va_arg(args, char *);
+    if (s == NULL)
+    {
+        s = "(null)";
     }
+    while (*s)
+    {
+        count += write(1, s, 1);
+        s++;
+    }
+    break;
 
-    // Clean up the va_list when done
-    va_end(args);
+				case '%':
+					count += write(1, &format[i], 1);
+					break;
+				case 'd':
+				case 'i':
+					d = va_arg(args, int);
+					count += print_number(d);
+					break;
+				default:
+					count += write(1, &format[i - 1], 1);
+					count += write(1, &format[i], 1);
+			}
+		}
+		else
+		{
+			count += write(1, &format[i], 1);
+		}
+		i++;
+	}
 
-    // Return the total number of characters printed
-    return printed_chars;
+	va_end(args);
+	return (count);
+}
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ * Return: the number of characters printed
+ */
+int print_number(int n)
+{
+	int count = 0;
+	unsigned int num;
+
+	if (n < 0)
+	{
+		count += write(1, "-", 1);
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+
+	if (num / 10)
+	{
+		count += print_number(num / 10);
+	}
+
+	count += write(1, &"0123456789"[num % 10], 1);
+
+	return (count);
 }
 
